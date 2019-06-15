d1224011f4179b8ee9df1401c3b9b024ddb6a4a0
Kunal Marwaha <marwahaha@berkeley.edu>
2019-06-15 13:20:16 -0700

    Fix scripts for non-cuda computer

--- experiments/exp1_airfoil/process_airfoil_shapes.py
+++ experiments/exp1_airfoil/process_airfoil_shapes.py
@@ -1,24 +1,26 @@
 from airfoil_NUFT import *
 import time
 
+print('Now processing airfoil shapes...')
+
 # Convert all airfoils in airfoil-data
-directory='data'    
+directory='data'
 i=0
 start=time.time()
 for root, dirs, files in os.walk(directory):
     for airfoil in dirs:
         if not os.path.exists('data/'+airfoil+'/seligdatfile'):
             print('WARNING: Selig .dat file for '+airfoil+' not found. Airfoil not converted and saved.')
-        else:                   
+        else:
             # Create save file name
             save_file='processed_data/'+airfoil+'.pt'
             if not os.path.exists(save_file):
-                airfoil_phys(airfoil, res=(224,224), save_name=save_file, grad=False)
+                airfoil_phys(airfoil, res=(224,224), device='cpu', save_name=save_file, grad=False)
             i=i+1
 
             if (i+1)%100==0:
                 end=time.time()
                 print(str(i+1)+' airfoils processed! Time elapsed: '+str(end-start))
                 start=time.time()
-                    
+
 print('Processing complete!')
--- experiments/exp1_airfoil/process_data.sh
+++ experiments/exp1_airfoil/process_data.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 rm -r processed_data
 mkdir processed_data
